ff7d93d00d2ee46e09f60936d2e5ccc8229379c7
abdullah034 <shaikhabdullah299@gmail.com>
2024-11-18 23:49:05 +0530

    Added hashchange and Load Event

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,10 +29,12 @@ const renderSpinner = function (parentelement) {
 
 const showrecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if(!id) return;
+    
     renderSpinner(recipeContainer);
-
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
 
@@ -156,4 +158,8 @@ const showrecipe = async function () {
     console.error(error);
   }
 };
-showrecipe();
+
+['hashchange','load'].forEach(event => window.addEventListener(event, showrecipe));
+
+// window.addEventListener('hashchange',showrecipe);
+// window.addEventListener('load',showrecipe);
